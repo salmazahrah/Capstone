0739c1b88ada7250138d7a6f802a410bb708f762
salmazahrah <158063604+salmazahrah@users.noreply.github.com>
2024-03-17 13:42:07 +0700

    portofolio_saham.py

--- portofolio_saham.py
+++ portofolio_saham.py
@@ -3,13 +3,18 @@ import pandas as pd
 import streamlit as st
 from pandas_datareader import data as wb
 from scipy.stats import norm
+from scipy.stats import genextreme
 from scipy.optimize import minimize
 import yfinance as yf
 import altair as alt
 import plotly.express as px
+import plotly.graph_objects as go
 
 # set bentuk halaman web
 st.set_page_config(layout='wide')
+
+# Aktifkan Streamlit debug mode
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 with st.sidebar:
     st.subheader("Penulis")
@@ -20,48 +25,94 @@ start_date = '2023-01-01'
 end_date = '2024-01-01'
 
 # Menggunakan fungsi DataReader dari yfinance
-data01 = yf.download("ALHC", start=start_date, end=end_date)
-data02 = yf.download("CELH", start=start_date, end=end_date)
-data03 = yf.download("FLNC", start=start_date, end=end_date)
+data01 = yf.download("BBRI.JK", start=start_date, end=end_date)
+data02 = yf.download("BBNI.JK", start=start_date, end=end_date)
+data03 = yf.download("BRIS.JK", start=start_date, end=end_date)
 
 
 # Inisialisasi DataFrame
 data00 = pd.DataFrame()
-data00['ALHC'] = data01['Adj Close']
-data00['CELH'] = data02['Adj Close']
-data00['FLNC'] = data03['Adj Close']
+data00['BBRI.JK'] = data01['Adj Close']
+data00['BBNI.JK'] = data02['Adj Close']
+data00['BRIS.JK'] = data03['Adj Close']
+
 data00.index = pd.to_datetime(data00.index)
 
 # Return saham harian
 r = data00.pct_change().dropna()
+returns_bbri = r['BBRI.JK']
+returns_bbni = r['BBNI.JK']
+returns_bris = r['BRIS.JK']
+    
+########## Perhitungan Parameter
+# BBRI
+gev_bbriarams_mle_bbri = genextreme.fit(returns_bbri)
+xi_bbri, mu_bbri, beta_bbri = gev_bbriarams_mle_bbri
+# BBNI
+gev_bbniarams_mle_bbni = genextreme.fit(returns_bbni)
+xi_bbni, mu_bbni, beta_bbni = gev_bbniarams_mle_bbni
+# BRIS
+gev_brisarams_mle_bris = genextreme.fit(returns_bris)
+xi_bris, mu_bris, beta_bris = gev_brisarams_mle_bris
 
-# Aktifkan Streamlit debug mode
-st.set_option('deprecation.showPyplotGlobalUse', False)
+########## Perhitungan iterasi dan Simulasi Monte Carlo
+alpha = 0.05
+z_score = 1.96  # Z-score untuk tingkat kepercayaan 95%
+# BBRI
+std_dev_bbri = np.std(returns_bbri)
+mean_bbri = np.mean(returns_bbri)
+iterations_bbri = ((100 * z_score * std_dev_bbri) / (5 * mean_bbri))**2
+# BBNI
+std_dev_bbni = np.std(returns_bbni)
+mean_bbni = np.mean(returns_bbni)
+iterations_bbni = ((100 * z_score * std_dev_bbni) / (5 * mean_bbni))**2
+# BRIS
+std_dev_bris = np.std(returns_bris)
+mean_bris = np.mean(returns_bris)
+iterations_bris = ((100 * z_score * std_dev_bris) / (5 * mean_bris))**2
+
+########### Simulasi Monte Carlo
+np.random.seed(42)
+def monte_carlo_simulation_genextreme(xi, mu, beta, returns, iterations):
+    VaR = np.percentile(
+        genextreme.rvs(xi, loc=mu, scale=beta, size=(iterations, len(returns))),
+        alpha * 100,
+        axis=1
+    )
+    return np.mean(VaR)
+
+# BBRI
+BBRI = monte_carlo_simulation_genextreme(xi_bbri, mu_bbri, beta_bbri, returns_bbri, int(iterations_bbri))
+# BBNI
+BBNI = monte_carlo_simulation_genextreme(xi_bbni, mu_bbni, beta_bbni, returns_bbni, int(iterations_bbni))
+# BRIS
+BRIS = monte_carlo_simulation_genextreme(xi_bris, mu_bris, beta_bris, returns_bris, int(iterations_bris))
+
 
 st.title("Estimasi Resiko Portofolio Saham")
 #####################################
 # Helper Function
 def format_big_number(num):
-    if num >= 1e6:
-        return f"{num / 1e6:.2f} Juta"
-    elif num >= 1e3:
-        return f"{num / 1e3:.2f} Ribu"
+    abs_num = abs(num)  # Ambil nilai absolut
+    if abs_num >= 1e6:
+        return f"{num / 1e6:.2f} Juta" if num >= 0 else f"-{abs_num / 1e6:.2f} Juta"
+    elif abs_num >= 1e3:
+        return f"{num / 1e3:.2f} Ribu" if num >= 0 else f"-{abs_num / 1e3:.2f} Ribu"
     else:
         return f"{num:.2f}"
-
+    
 ###### Tabbing plot saham
 tab1, tab2, tab3, tab4 = st.tabs(['PORTOFOLIO','Daily Return', 'Matriks Korelasi', 'Simulasi'])
 
 with tab1:
-    
     with st.expander("Data Harga Closing Saham"):
         st.write(data00.head())
-        st.caption("[finance.yahoo.com](https://finance.yahoo.com/quote/ALHC/history)")
+        st.caption("[finance.yahoo.com](https://finance.yahoo.com/quote/BBRI.JK/history)")
         
     st.write("Grafik Harga Closing Saham")
     data = st.multiselect(
         "Pilih Saham",
-        ['ALHC','CELH','FLNC'],
+        ['BBRI.JK','BBNI.JK','BRIS.JK'],
         key="multiselect_1"
         )
     selected_data = data00[data].reset_index()
@@ -70,13 +121,13 @@ with tab1:
     
     st.write("Statistika Deskriptif")
     st.write(data00.describe())
-        
-    
+            
 with tab2:
     st.write("Dailyl Returns")
+    st.write(r)
     data2 = st.multiselect(
         "Pilih Saham",
-        ['ALHC','CELH','FLNC'],
+        ['BBRI.JK','BBNI.JK','BRIS.JK'],
         key="multiselect_2"
     )
     returns = r[data2].reset_index()
@@ -125,94 +176,43 @@ with tab3:
     st.markdown("__Matrix Korelasi__")
     st.write("""
         Dengan menggunakan matriks korelasi, kita dapat melihat hubungan antara harga penutupan saham
-        1. Dapat dilihat bahwa nilai korelasi saham CELH dan FLNC bernilai positif, ini menunjukkan bahwa saham CELH bergerak dalam arah yang sama
-        2. Dapat dilihat bahwa nilai korelasi saham ALHC dengan CELH dan FLNC bernilai negatif, ini menujukkan bahwa saham ALHC cenderung bergerak kearah yang berlawanan dengan saham CELH dan FLNC
+        1. Dapat dilihat bahwa nilai korelasi saham BBNI.JK dengan BRIS.JK bernilai positif 0.24, ini menunjukkan bahwa saham BBNI.JK dan BRIS.JK memiiki hubungan yang lemah
+        2. Dapat dilihat bahwa nilai korelasi saham BBRI.JK dengan BBNI.JK bernilai positif 0.43, ini menujukkan bahwa saham BBRI.JK dan BBNI.JK memiliki hubungan yang sedang
+        3. Dapat dilihat bahwa nilai korelasi saham BBRI.JK dengan BBRIS.JK bernilai positif 0.70, ini menujukkan bahwa saham BBRI.JK dan BBNI.JK memiliki hubungan yang kuat
     """)    
     
 with tab4:
     st.write("Simulasi")
-    ############### Proporsi Saham pada Portofolio
-    # Kolom
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        p_alhc = st.number_input("Proporsi Saham ALHC", min_value=0.0, max_value=1.0, step=0.01, value=0.33)
+    # Proporsi Saham pada Portofolio
+    p_bbri = 0.33
+    p_bbni = 0.51
+    p_bris = 0.16
 
-    with col2:
-        p_celh = st.number_input("Proporsi Saham CELH", min_value=0.0, max_value=1.0, step=0.01, value=0.51)
+    data00['porto'] = ((p_bbri * data00['BBRI.JK']) + (p_bbni * data00['BBNI.JK']) + (p_bris * data00['BRIS.JK']))    
+    r['PORTOFOLIO'] = data00['porto'].pct_change().dropna()
+    returns_porto = r['PORTOFOLIO']
 
-    with col3:
-        p_flnc = st.number_input("Proporsi Saham FLNC", min_value=0.0, max_value=1.0, step=0.01, value=0.16)
+    gev_params_mle_porto = genextreme.fit(returns_porto)
+    xi_porto, mu_porto, beta_porto = gev_params_mle_porto
 
-    # Periksa apakah total proporsi adalah 1
-    total_proporsi = p_alhc + p_celh + p_flnc
+    # Perhitungan iterasi
+    std_dev_porto = np.std(returns_porto)
+    mean_porto = np.mean(returns_porto)
+    iterations_porto = ((100 * z_score * std_dev_porto) / (5 * mean_porto))**2
 
-    # Tampilkan pesan jika total proporsi tidak sama dengan 1
-    if total_proporsi != 1:
-        st.warning(f"Total proporsi saat ini adalah {total_proporsi:.2f}. Pastikan total proporsi sama dengan 1.")
-
-    data00['porto'] = ((p_alhc * data00['ALHC']) + (p_celh * data00['CELH']) + (p_flnc * data00['FLNC']))    
-    # Fungsi untuk menjalankan simulasi Monte Carlo
-    def monte_carlo_simulation(initial_price, drift, volatility, time_steps, num_simulations):
-        dt = 1 / 252  # perhitungan per hari dalam setahun
-        simulations = []
-
-        for _ in range(num_simulations):
-            daily_returns = np.exp((drift - 0.5 * volatility**2) * dt + volatility * np.sqrt(dt) * np.random.normal(0, 1, time_steps))
-            stock_price = initial_price * np.cumprod(daily_returns)
-            simulations.append(stock_price)
-
-        return np.array(simulations)
-
-
-    ## Parameter-model
-    return_daily = data00['porto'].pct_change().dropna()
-    return_daily.index = pd.to_datetime(return_daily.index)
-    expected_return = return_daily.mean()
-    var = return_daily.var()
-    drift = expected_return - (0.5 * var) # tingkat drift harian (sesuai dengan data historis)
-    volatility = return_daily.std() # volatilitas harian (sesuai dengan data historis)
-    initial_price = data00['porto']  # harga saham awal
-    time_steps = len(data00)  # jumlah langkah waktu (dalam satu tahun)
-    num_simulations = 1000
-
-    # Jalankan simulasi Monte Carlo
-    simulations = monte_carlo_simulation(initial_price, drift, volatility, time_steps, num_simulations)
-
-    ############# Menghitung VaR
-    # selectbox menampilkan Tingkat Kepercayaan dropdown untuk dipilih
-    alpha = st.selectbox(
-        "Pilih Tingkat Kepercayaan yang akan digunakan (%)",
-        ['1','5','10']
-    )
-    alpha = float(alpha)  # Mengonversi alpha menjadi float
-    tingkat_kepercayaan = 100 - alpha
-
-    # number_input untuk input number
-    investasi = st.number_input(
-        "Jumlah investasi awal",
-        min_value=0,
-        max_value=999999999,
-        step=1,
-        value=100000000
-    )
-    var_percentile = np.percentile(simulations[:, -1], alpha)
-    hasil = investasi * var_percentile/100
-
-    st.write(f"Estimasi resiko dengan tingkat kepercayaan {tingkat_kepercayaan}% setelah dilakukan 1000 kali iterasi sebesar {format_big_number(var_percentile)}%")
-
-    st.write(f"Jika investasi awal sebesar {investasi} dengan tingkat kepercayaan {tingkat_kepercayaan}% di hari selanjutnya mendapatkan hasil sebesar {format_big_number(hasil)}")
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
+    # Simulasi Monte Carlo
+    np.random.seed(42)
+    PORTOFOLIO = monte_carlo_simulation_genextreme(xi_porto, mu_porto, beta_porto, returns_porto, int(iterations_porto))
+        
+    investasi_awal = 100000000
+    hasil_bbri = investasi_awal * BBRI
+    hasil_bbni = investasi_awal * BBNI
+    hasil_bris = investasi_awal * BRIS
+    hasil_porto = investasi_awal * PORTOFOLIO
+    
+    hasil_investasi = pd.DataFrame({
+        'Saham': ['BBRI', 'BBNI', 'BRIS', 'PORTOFOLIO'],
+        'Value_at_Risk (%)': [BBRI, BBNI, BRIS, PORTOFOLIO],
+        'Hasil_Investasi': [round(hasil_bbri), round(hasil_bbni), round(hasil_bris), round(hasil_porto)]
+    })
+    st.write("Tabel Hasil Investasi", hasil_investasi)
